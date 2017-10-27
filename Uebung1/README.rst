eb5cde55c54ea1e393cbbfbd693e6b8f1447269b
marius_koeppel <koeppel.ma@googlemail.com>
2017-10-27 19:53:45 +0200

    working implementation of apriori

--- Uebung1/README.rst
+++ Uebung1/README.rst
@@ -8,3 +8,13 @@ Installation
 ------------
 
 - Run :code:`python setup.py install`.
+
+
+"""
+Description     : Simple Python implementation of the Apriori Algorithm
+
+Usage:
+    $python apriori.py -f DATASET.csv -s minSupport  -c minConfidence
+
+    $python apriori.py -f DATASET.csv -s 0.15 -c 0.6
+"""
--- Uebung1/src/main.py
+++ Uebung1/src/main.py
@@ -1,80 +1,47 @@
 # -*- coding: utf-8 -*-
 # Main file
-# ToDo:
-
-def combination(l, r):
-    if r == 0:
-        yield []
-    elif len(l) == r:
-        yield l
-    else:
-        for c in (combination(l[1:], r-1)):
-            yield l[0:1]+c
-        for c in (combination(l[1:], r)):
-            yield c
-
-def generateNthCandidates(NthTransactions, listOfCombinations, nthCombi, support_thres):
-    import pandas as pd
-
-    for combination in listOfCombinations:
-        nameColumn = str(combination)
-        defaultColumn = []
-        for i in range(NthTransactions.shape[1]):
-            defaultColumn.append(0)
-        dfTmp = pd.DataFrame({nameColumn: defaultColumn})
-        for column in combination:
-            dfTmp[nameColumn] += NthTransactions[int(column)]
-        dfTmp[nameColumn] = (dfTmp[nameColumn]/nthCombi).astype(int)
-        print dfTmp[nameColumn]
-        NthTransactions.join(dfTmp[nameColumn])
-
-    generateFirstCandidates(NthTransactions, support_thres)
-
-def generateFirstCandidates(transactions, support_thres):
-    for i in transactions.sum().iteritems():
-        if float(i[1])/transactions.shape[0] < support_thres:
-            del transactions[i[0]]
-    return transactions
-
-
-
 
 def main():
     import sys
-    from reader import reader
-    from apriori import apriori
+    from optparse import OptionParser
+    from apriori import *
 
-    # Method to generate the candidates
-    # sys.argv[1]
-    data = "../data/dm3.csv"
-    reader = reader(data)
-    transactions = reader.readConvertCsv()
+    optparser = OptionParser()
+    optparser.add_option('-f', '--inputFile',
+                         dest='input',
+                         help='filename containing csv',
+                         default=None)
+    optparser.add_option('-s', '--minSupport',
+                         dest='minS',
+                         help='minimum support value',
+                         default=0.15,
+                         type='float')
+    optparser.add_option('-c', '--minConfidence',
+                         dest='minC',
+                         help='minimum confidence value',
+                         default=0.6,
+                         type='float')
 
-    # Delete the first transactions
-    FirstTransactions = generateFirstCandidates(transactions, 0.4)
+    (options, args) = optparser.parse_args()
 
-    # Generate nth combination and delete if under support
-    for nthCombi in range(FirstTransactions.shape[1]):
-        listOfCombinations = []
-        combinations = combination(list(FirstTransactions.columns), nthCombi + 2)
-        for j in combinations:
-            listOfCombinations.append(j)
-        FirstTransactions = generateNthCandidates(FirstTransactions, listOfCombinations, nthCombi, 0.4)
+    inFile = None
+    if options.input is None:
+        inFile = sys.stdin
+    elif options.input is not None:
+        inFile = dataFromFile(options.input)
+    else:
+        print ('No dataset filename specified, system with exit\n')
+        sys.exit('System will exit')
 
+    minSupport = options.minS
+    minConfidence = options.minC
 
+    items, rules = runApriori(inFile, minSupport, minConfidence)
 
+    printResults(items, rules)
 
-
-    """
-    Keyword arguments:
-    min_support -- The minimum support of relations (float).
-    min_confidence -- The minimum confidence of relations (float).
-    min_lift -- The minimum lift of relations (float).
-    max_length -- The maximum length of the relation (integer).
-
-    kwargs = {'min_support': 0.9, 'min_confidence': 0.0, 'min_lift': 0.0, 'max_length': None}
-    print (list(apriori(transaction, **kwargs)))
-        """
 
 if __name__ == "__main__":
-    main()
+    import profile
+    profile.run('main()')
+
