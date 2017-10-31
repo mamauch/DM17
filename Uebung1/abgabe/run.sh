cce5e5478519ea1b3b4d2c280e558d5ef089f85f
marius_koeppel <koeppel.ma@googlemail.com>
2017-10-31 10:11:39 +0100

    add run.sh for no2

---
+++ Uebung1/abgabe/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+python create_hist_nr2.py ../output/dm1.csv_0.4.csv
+python create_hist_nr2.py ../output/dm1.csv_0.5.csv
+python create_hist_nr2.py ../output/dm1.csv_0.6.csv
+python create_hist_nr2.py ../output/dm1.csv_0.7.csv
+python create_hist_nr2.py ../output/dm1.csv_0.8.csv
+python create_hist_nr2.py ../output/dm1.csv_0.9.csv
+
+python create_hist_nr2.py ../output/dm2.csv_0.4.csv
+python create_hist_nr2.py ../output/dm2.csv_0.5.csv
+python create_hist_nr2.py ../output/dm2.csv_0.6.csv
+python create_hist_nr2.py ../output/dm2.csv_0.7.csv
+python create_hist_nr2.py ../output/dm2.csv_0.8.csv
+python create_hist_nr2.py ../output/dm2.csv_0.9.csv
+
+python create_hist_nr2.py ../output/dm3.csv_0.4.csv
+python create_hist_nr2.py ../output/dm3.csv_0.5.csv
+python create_hist_nr2.py ../output/dm3.csv_0.6.csv
+python create_hist_nr2.py ../output/dm3.csv_0.7.csv
+python create_hist_nr2.py ../output/dm3.csv_0.8.csv
+python create_hist_nr2.py ../output/dm3.csv_0.9.csv
+
+python create_hist_nr2.py ../output/movielens.bin_0.4.csv
+python create_hist_nr2.py ../output/movielens.bin_0.5.csv
+python create_hist_nr2.py ../output/movielens.bin_0.6.csv
+python create_hist_nr2.py ../output/movielens.bin_0.7.csv
+python create_hist_nr2.py ../output/movielens.bin_0.8.csv
+python create_hist_nr2.py ../output/movielens.bin_0.9.csv
